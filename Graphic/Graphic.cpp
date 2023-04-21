0df4a70c2692ace8168f972f7d866b259587ea7e
Louis Grange <louisgrange@ik.me>
2023-04-21 14:19:03 +0200

    Pointeur Cairo static

--- Graphic/Graphic.cpp
+++ Graphic/Graphic.cpp
@@ -8,6 +8,10 @@
 
 static const Cairo::RefPtr<Cairo::Context>* ptcr(nullptr);
 
+void graphic_set_context(const Cairo::RefPtr<Cairo::Context>& cr) {
+    ptcr = &cr;
+}
+
 void graphic_draw_shape(const int width, const int height) {
-    
+
 }
